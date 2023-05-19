c4f2138f875ab80208da7c7f7a133c05bac52e9e
Swapnil-FWDeveloper <swapnilshende9284@gmail.com>
2023-05-19 16:55:14 +0530

    refacter adding new project

---
+++ my-app/src/Component/Player.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const Player = () => {
+    const[audio,setAudio]=useState(null);
+    const [track,setTrack]=useState(null)
+    const [duration,setDuration]=useState(null)
+    const [curtime,setCurtime]=useState(null)
+    const [timingplaying,setTimeplaying]=useState(false)
+    
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Player
