ac545ba9990d0f2fad00d5799d7bad549e348470
Swapnil-FWDeveloper <swapnilshende9284@gmail.com>
2023-05-21 22:14:07 +0530

    new file

--- my-app/src/pages/Discover.jsx
+++ my-app/src/pages/Discover.jsx
@@ -1,22 +1,40 @@
-import { Error, Loader, SongCard } from '../components';
-import { genres } from '../assets/constants';
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
 
 const Discover = () => {
-//   console.log(genres);
-  const genreTitle = 'Pop';
+  //   console.log(genres);
+  const genreTitle = "Pop";
   return (
     <div className="flex flex-col">
       <div
         className="w-full flex justify-between items-center
-         sm:flex-row flex-col mt-4 mb-10 ">
-         <h2 className = "font-bolt text-3xl text-white text-left ">Discover {genreTitle}</h2>
-            <select onChange={()=>{}}
-            value=""
-            className = "bg-black"
-            >
+         sm:flex-row flex-col mt-4 mb-10 "
+      >
+        <h2 className="font-bolt text-3xl text-white text-left ">
+          Discover {genreTitle}
+        </h2>
+        <select
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 p-3 text-smrounded-lg outline-none sm:mt-0 mt-5"
+        >
+          {/* here we are maping the data of genres to see the valuse inside select tag (pop) */}
+          {genres.map((genres) => (
+            <option key={genres.value}>{genres.title}</option>
+          ))}
+        </select>
+      </div>
 
-            </select>
-         </div>
+      {/* wripper of our songs */}
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+      {[1,2,3,4,5,6,7,8,9,10].map((song,i)=>(
+        <SongCard key={song.key}
+        song={song}
+        i={i}
+        />
+      ))}
+
+      </div>
     </div>
   );
 };
---
+++ my-app/src/redux/services/shazamCore.js
@@ -0,0 +1,4 @@
+// In this folder we are going to maintain api call's
+// RapidAPI does not have a specific "Shazane Core" API. Shazam is a popular music identification service
+// that allows users to identify songs by listening to a short sample of the audio. However, 
+// the Shazam API itself is not publicly available on RapidAPI.
